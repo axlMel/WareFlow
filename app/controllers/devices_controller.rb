bccb4266d43df1fca277fed86a27934fd90ea280
axlMel <david.lexf@gmail.com>
2026-03-05 18:30:14 -0600

    FASE 1.6 sims

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,6 +1,11 @@
 class DevicesController < ApplicationController
-	def index
+  before_action :set_device, only: [:show, :edit, :update, :destroy]
+
+  def index
     @devices = Device.includes(:product).order(created_at: :desc)
+  end
+
+  def show
   end
 
   def new
@@ -17,7 +22,27 @@ class DevicesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @device.update(device_params)
+      redirect_to devices_path, notice: "Device actualizado"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @device.destroy
+    redirect_to devices_path, notice: "Device eliminado"
+  end
+
   private
+
+  def set_device
+    @device = Device.find(params[:id])
+  end
 
   def device_params
     params.require(:device).permit(:imei, :brand, :model, :product_id)
--- app/controllers/sims_controller.rb
+++ app/controllers/sims_controller.rb
@@ -1,6 +1,18 @@
 class SimsController < ApplicationController
-	def index
-    @sims = Sim.order(created_at: :desc)
+  before_action :set_sim, only: [:show, :edit, :update, :destroy]
+
+  def index
+    finder = FindSims.new(Sim.all, params)
+    scoped = finder.call
+
+    sort_column = params[:sort] || 'created_at'
+    sort_direction = params[:direction] == 'desc' ? 'desc' : "asc"
+    scoped = scoped.order("#{sort_column} #{sort_direction}")
+
+    @pagy, @sims = pagy(scoped, items: params[:per_page] || 10)
+  end
+
+  def show
   end
 
   def new
@@ -17,7 +29,27 @@ class SimsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @sim.update(sim_params)
+      redirect_to sims_path, notice: "SIM actualizada"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @sim.destroy
+    redirect_to sims_path, notice: "SIM eliminada"
+  end
+
   private
+
+  def set_sim
+    @sim = Sim.find(params[:id])
+  end
 
   def sim_params
     params.require(:sim).permit(:iccid, :provider, :apn, :phone_number, :product_id)
